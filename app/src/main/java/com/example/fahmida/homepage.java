93879abbf1c8742f5c5ec8b418a9ade9c47b1576
rameem2003 <rameem2019@gmail.com>
2023-04-11 22:27:22 +0600

    one field added

--- app/src/main/java/com/example/fahmida/homepage.java
+++ app/src/main/java/com/example/fahmida/homepage.java
@@ -6,9 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class homepage extends AppCompatActivity {
-    Button btn;
+    Button btn, getName;
+    TextInputEditText input;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +21,23 @@ public class homepage extends AppCompatActivity {
         setContentView(R.layout.activity_homepage);
 
         btn = findViewById(R.id.btn);
+        input = findViewById(R.id.input);
+        getName = findViewById(R.id.getName);
+        text = findViewById(R.id.text);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent a = new Intent(homepage.this,about.class);
                 startActivity(a);
+            }
+        });
+
+        getName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String value = input.getText().toString();
+                text.setText(value);
             }
         });
     }
