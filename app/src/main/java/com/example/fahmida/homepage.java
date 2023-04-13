f7193ef881c8e124637259d46b6ab98802560877
rameem2003 <rameem2019@gmail.com>
2023-04-13 22:53:15 +0600

    stable release

--- app/src/main/java/com/example/fahmida/homepage.java
+++ app/src/main/java/com/example/fahmida/homepage.java
@@ -11,33 +11,50 @@ import android.widget.TextView;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class homepage extends AppCompatActivity {
-    Button btn, getName;
-    TextInputEditText input;
-    TextView text;
+    TextInputEditText firstNumber, secondNumber;
+
+    Button sum;
+    TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
 
-        btn = findViewById(R.id.btn);
-        input = findViewById(R.id.input);
-        getName = findViewById(R.id.getName);
-        text = findViewById(R.id.text);
+//        btn = findViewById(R.id.btn);
+//        input = findViewById(R.id.input);
+//        getName = findViewById(R.id.getName);
+//        text = findViewById(R.id.text);
+//
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent a = new Intent(homepage.this,about.class);
+//                startActivity(a);
+//            }
+//        });
+//
+//        getName.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String value = input.getText().toString();
+//                text.setText(value);
+//            }
+//        });
+        firstNumber = findViewById(R.id.firstnumber);
+        secondNumber = findViewById(R.id.secondnumber);
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent a = new Intent(homepage.this,about.class);
-                startActivity(a);
-            }
-        });
+        sum = findViewById(R.id.sumBtn);
+        result = findViewById(R.id.result);
 
-        getName.setOnClickListener(new View.OnClickListener() {
+        sum.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String value = input.getText().toString();
-                text.setText(value);
+                int number1 = Integer.parseInt(firstNumber.getText().toString());
+                int number2 = Integer.parseInt(secondNumber.getText().toString());
+                int sumResult = number1 + number2;
+
+                result.setText(String.valueOf(sumResult));
             }
         });
     }
